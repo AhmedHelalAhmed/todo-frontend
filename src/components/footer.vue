c5a4c17ffb627a033e1a61964aed80cfe4611117
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-05-30 18:42:11 +0200

    init

---
+++ src/components/footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer-copyright text-center">
+    <div class="text-center">
+      © 2020
+      <a href="https://github.com/AhmedHelalAhmed">Ahmed Helal Ahmed</a> All rights
+      reserved.
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+footer div {
+  height: 100%;
+  background-color: #333;
+  color: rgba(255, 255, 255, 0.5);
+  display: block;
+}
+footer,
+::after,
+::before {
+  box-sizing: border-box;
+}
+
+footer a {
+  color: #f3e9e9;
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  line-height: 3rem;
+  background-color: #f5f5f5;
+}
+</style>
